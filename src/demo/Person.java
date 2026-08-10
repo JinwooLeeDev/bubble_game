ec5626c575824969cc3cf676e8a5941959743c92
I3abel <jinwoo_dev@naver.com>
2026-08-10 15:46:44 +0900

    test02 시작

---
+++ src/demo/Person.java
@@ -0,0 +1,19 @@
+package demo;
+
+import lombok.*;
+
+//@Getter
+//@Setter
+//@ToString
+// 단축 버전
+@Data
+/////////////////////
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Person {
+
+    private String name;
+    private Integer age;
+
+}
